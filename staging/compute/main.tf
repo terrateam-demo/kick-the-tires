ef3a1e2f3bddccb6254838826fae561e0eb898b9
BBR <bender@terrateam.io>
2025-03-06 15:17:07 +0100

    1741270627

--- staging/compute/main.tf
+++ staging/compute/main.tf
@@ -1,7 +1,7 @@
 module "staging_compute_instance" {
   source = "../../modules/compute"
 
-  instance_count = 2
+  instance_count = 0
   instance_name  = "staging-instance"
   instance_zone  = "us-central1-a"
   boot_disk_image = "debian-cloud/debian-11"
